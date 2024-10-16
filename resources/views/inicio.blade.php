39e00c68a6f055a580c9de42dd35e76725579e7f
fabia <fabianhdz981@gmail.com>
2024-10-16 13:38:00 -0500

    actualizar

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
-    @vite(['resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     @if(Auth::check()) <!-- Verifica si el usuario está autenticado -->
